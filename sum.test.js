801b6844720201b54d68c6e11c50997b1932338c
Jonathan Cain <jcainuk@gmail.com>
2022-04-08 18:02:11 +0100

    add info about toBe matcher and toEqual matcher

--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,19 @@
 // require the sum.js file
 const sum = require('./sum');
 
-describe('', () => {
+describe('example tests', () => {
   test('should add 1 + 2 to equal 3', () => {
     const result = sum(1, 2);
     expect(result).toBe(3);
+  });
+
+  // test('object assignment', () => {
+  //   const obj = {};
+  //   expect(obj).toBe({}); THIS FAILS :(
+  // }
+
+  test('object assignment', () => {
+    const obj = {};
+    expect(obj).toEqual({});// THIS PASSES :)
   });
 });
