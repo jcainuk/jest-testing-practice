ed5fa99c03e82ef766e67cc9e906a8e301c8880d
Jonathan Cain <jcainuk@gmail.com>
2022-04-08 17:56:28 +0100

    wrapped test in a describe block

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,9 @@
 // require the sum.js file
 const sum = require('./sum');
 
-test('should add 1 + 2 to equal 3', () => {
-  const result = sum(1, 2);
-  expect(result).toBe(3);
+describe('', () => {
+  test('should add 1 + 2 to equal 3', () => {
+    const result = sum(1, 2);
+    expect(result).toBe(3);
+  });
 });
