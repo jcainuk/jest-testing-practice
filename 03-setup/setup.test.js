3b0402da01237bb0a303ae3ae4a0a757d96c604c
Jonathan Cain <jcainuk@gmail.com>
2022-05-06 16:29:40 +0100

    start setup test file to practise setup and teardown tests

---
+++ 03-setup/setup.test.js
@@ -0,0 +1,7 @@
+const animals = ['elephant', 'zebra', 'bear', 'tiger'];
+
+describe('animals array', () => {
+  it('should add an animal to the end of array', () => {
+    animals.push('aligator');
+  });
+});
