3402e8f6f52477f63f415918aed4badd3df508d1
Jonathan Cain <jcainuk@gmail.com>
2022-05-06 16:32:17 +0100

    wrote assertion for setup test

--- 03-setup/setup.test.js
+++ 03-setup/setup.test.js
@@ -3,5 +3,6 @@ const animals = ['elephant', 'zebra', 'bear', 'tiger'];
 describe('animals array', () => {
   it('should add an animal to the end of array', () => {
     animals.push('aligator');
+    expect(animals[animals.length - 1]).toBe('aligator');
   });
 });
