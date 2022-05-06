363f445cadb8d8b0cc1e43bfe5fa8af950cbbd54
Jonathan Cain <jcainuk@gmail.com>
2022-05-06 16:23:57 +0100

    refactor async test to make it cleaner

--- 02-async/async.test.js
+++ 02-async/async.test.js
@@ -1,7 +1,6 @@
 const fetchData = require('./async');
 
-it('should return correct todo', () => {
-  fetchData(1).then((todo) => {
-    expect(todo.id).toBe(1);
-  });
+it('should return correct todo', async () => {
+  const todo = await fetchData(1);
+  expect(todo.id).toBe(1);
 });
