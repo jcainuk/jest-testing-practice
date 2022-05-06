b3073fe38d76d9e658df34da9664fc3be243f750
Jonathan Cain <jcainuk@gmail.com>
2022-05-06 16:04:05 +0100

    edit pseudo code info

--- 02-async/async.js
+++ 02-async/async.js
@@ -2,7 +2,7 @@
 const axios = require('axios');
 
 const fetchData = (id) => {
-  // {JSON} Placeholder is a free fake API for testing and prototyping.
+  // https://jsonplaceholder.typicode.com is a free fake API for testing and prototyping.
 
   const results = axios.get(`https://jsonplaceholder.typicode.com/todo/${id}`);
 };
