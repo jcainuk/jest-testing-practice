c2f159b78d6c0160d091b5456de74eb3bfa056bd
Jonathan Cain <jcainuk@gmail.com>
2022-05-09 15:41:38 +0100

    write an example mock function

--- 04-mocks/mocks.test.js
+++ 04-mocks/mocks.test.js
@@ -3,3 +3,12 @@ const forEach = (items, callback) => {
     callback(items[i]);
   }
 };
+
+it('mock callback', () => {
+  const mockCalledback = jest.fn((x) => 42 + x);
+
+  forEach([0, 1], mockCalledback);
+  // Access the .mock property
+  // Expect the number of times the function was called the 2
+  expect(mockCalledback.mock.calls.length).toBe(2);
+});
