cbb0b7a300a6778057034ef2eed9a9dd2ac0e056
Jonathan Cain <jcainuk@gmail.com>
2022-05-09 15:54:56 +0100

    write more pseudo code and mock.results.value tests

--- 04-mocks/mocks.test.js
+++ 04-mocks/mocks.test.js
@@ -10,5 +10,16 @@ it('mock callback', () => {
   forEach([0, 1], mockCalledback);
   // Access the .mock property
   // Expect the number of times the function was called the 2
+  // .calls is in fact an array e.g. [ [ call1 ], [ call2 ] ]
   expect(mockCalledback.mock.calls.length).toBe(2);
+
+  expect(mockCalledback.mock.calls[0][0]).toBe(0);
+
+  expect(mockCalledback.mock.calls[1][0]).toBe(1);
+
+  // value of first item in array after function call
+  expect(mockCalledback.mock.results[0].value).toBe(42); // 0 + 42
+
+  // value of second item in array after function call
+  expect(mockCalledback.mock.results[1].value).toBe(43); // 1 + 42
 });
